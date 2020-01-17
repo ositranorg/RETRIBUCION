61ba2e928df937d76522e44c6397583e2f5fb93d
Miguel Angel Huamani Mendoza <mhuamani.a.m@gmail.com>
2020-01-17 00:10:41 -0500

    lista de contribuyentes

--- pyRetribucion/src/main/java/com/kemal/spring/web/controllers/restApiControllers/ContribuyenteRestController.java
+++ pyRetribucion/src/main/java/com/kemal/spring/web/controllers/restApiControllers/ContribuyenteRestController.java
@@ -3,6 +3,7 @@ package com.kemal.spring.web.controllers.restApiControllers;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kemal.spring.service.ContribuyenteService;
-import com.kemal.spring.web.controllers.restApiControllers.dto.UsuarioDto;
+import com.kemal.spring.web.controllers.restApiControllers.dto.ContribuyenteDto;
 
 @RestController
 @RequestMapping("api/contribuyente")
@@ -22,11 +23,22 @@ public class ContribuyenteRestController {
 	@Autowired
 	ContribuyenteService contribuyenteService;
 	
+	@Value("${total-registro-por-pagina}")
+	private int totalRegistroPorPagina;
+	
 	@ResponseBody
 	@PostMapping(value = "listar-contribuyente", consumes = "application/json",produces =  { "application/json" })
 	public ResponseEntity<?> listarContribuyente() {
 		HashMap<String, Object> rest = new HashMap<String, Object>();
 		rest.put("lista", contribuyenteService.listaContribuyente());
+		rest.put("resultado", 1);
+		return new ResponseEntity<>(rest, HttpStatus.OK);
+	}
+	@ResponseBody
+	@PostMapping(value = "listar-contribuyente-paginacion", consumes = "application/json",produces =  { "application/json" })
+	public ResponseEntity<?> listarContribuyentePaginacion(@RequestBody ContribuyenteDto contribuyenteDto) {
+		HashMap<String, Object> rest = new HashMap<String, Object>();
+		rest.put("lista", contribuyenteService.listaContribuyentePaginacion(contribuyenteDto.getPagina(), totalRegistroPorPagina));
 		rest.put("resultado", 1);
 		return new ResponseEntity<>(rest, HttpStatus.OK);
 	}
---
+++ pyRetribucion/src/main/java/com/kemal/spring/web/controllers/restApiControllers/dto/ContribuyenteDto.java
@@ -0,0 +1,8 @@
+package com.kemal.spring.web.controllers.restApiControllers.dto;
+
+import lombok.Data;
+
+@Data
+public class ContribuyenteDto {
+	private int pagina;
+}
