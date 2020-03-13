05a52aec2cd40e6b28df6b0e6ad7e0cc514ec0d7
ubuntu <ubuntu@oraclepc>
2020-03-12 20:12:43 -0500

    permisos pack

--- BD/permisos.sql
+++ BD/permisos.sql
@@ -1,1 +1,4 @@
-grant select on SCINVGSF.t_concesionario to sretessf1;
+--con el usuario scinvgsf
+grant references on SCINVGSF.t_concesionario to sretessf1;
+grant select on SAPREGSF.SAPR_TIPODECLARACION to sretessf1;
+grant references on SAPREGSF.SAPR_TIPODECLARACION to sretessf1;
